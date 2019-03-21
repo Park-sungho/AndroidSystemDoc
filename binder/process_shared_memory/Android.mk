65e34987c89f0b817bfb66d7807faab09d17da41
Sungho <skyreon@gmail.com>
2019-03-21 13:34:13 +0900

    Update Android.mk

--- binder/process_shared_memory/Android.mk
+++ binder/process_shared_memory/Android.mk
@@ -4,13 +4,14 @@ include $(CLEAR_VARS)
 # ========================================================
 # Read Shared Memory Main Test 
 # ========================================================
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= read_binder_process.c
 LOCAL_MODULE := read_binder_process
-LOCAL_C_INCLUDES := $(COMMON_INCLUDES) $(KERNEL_INICLUDES)
-LOCAL_SHARED_LIBRARIES:= $(COMMON_LIBS)
-LOCAL_MODULE_PATH	:= $(LOCAL_PATH)/bin
-LOCAL_MODULE_TAGS	:= optional
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils
+
+LOCAL_MODULE_PATH   := $(LOCAL_PATH)/bin
+LOCAL_MODULE_TAGS   := optional
 include $(BUILD_EXECUTABLE)
 
 # ========================================================
@@ -19,9 +20,10 @@ include $(BUILD_EXECUTABLE)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES:= write_binder_process.c
 LOCAL_MODULE := write_binder_process
-LOCAL_C_INCLUDES := $(COMMON_INCLUDES) $(KERNEL_INICLUDES)
-LOCAL_SHARED_LIBRARIES:= $(COMMON_LIBS)
-LOCAL_MODULE_PATH	:= $(LOCAL_PATH)/bin
-LOCAL_MODULE_TAGS	:= optional
-include $(BUILD_EXECUTABLE)
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils
 
+LOCAL_MODULE_PATH   := $(LOCAL_PATH)/bin
+LOCAL_MODULE_TAGS   := optional
+include $(BUILD_EXECUTABLE)
