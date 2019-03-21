1da4c415f86880f241ecdac5d820788c0c1f251a
Sungho <skyreon@gmail.com>
2019-03-21 13:36:01 +0900

    Update read_binder_process.c

--- binder/process_shared_memory/read_binder_process.c
+++ binder/process_shared_memory/read_binder_process.c
@@ -11,14 +11,16 @@ int main() {
     char *buffer;
 
     /* int ashmem_create_region(const char *name, size_t size) */
-    ashId = ashmem_create_region(OEM_SHARED_MEMORY_NAME, OEM_SHARED_MEMORY_SIZE);
+    ashId = ashmem_create_region(OEM_SHARED_MEMORY_NAME,
+                  OEM_SHARED_MEMORY_SIZE);
     if (ashId < 0) {
         printf("ashmem_create_region is failed\n");
         return -1; 
     }    
     
     /* mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0) */
-    buffer = (char*)mmap(NULL, OEM_SHARED_MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, ashId, 0); 
+    buffer = (char*)mmap(NULL, OEM_SHARED_MEMORY_SIZE, 
+                  PROT_READ | PROT_WRITE, MAP_SHARED, ashId, 0); 
     if (buffer < 0) {
         printf("mmap: %s is failed.\n", buffer);
         return -1; 
